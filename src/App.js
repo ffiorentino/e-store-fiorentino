56ccb02696be859998fb483859639a6380196c7e
fernando.fiorentino <fernando.fiorentino@FFIORENTINO-NB1>
2022-07-15 00:17:24 -0300

    Desafio Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      
+      <NavBar></NavBar>
     </div>
   );
 }
